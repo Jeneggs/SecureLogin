4f26053bb6d9e7a15307511d77c3a8a20e238c7b
Jeneggs <jenexnakaisumo@gmail.com>
2025-09-17 18:46:54 +0800

    feat(frontend): form creation

--- login-page.php
+++ login-page.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +11,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="" method="POST">
+        <label for="Username">Username:</label>
+        <input type="text" id="Username" name="Username" required>
+        <br>
+
+        <label for="Password">Password:</label>
+        <input type="password" id="Password" name="Password" required>
+        <br>
+
+        <button type="submit">Login</button>
+        <button type="submit">Sign Up</button>
+    </form>
+
 </body>
 </html>
