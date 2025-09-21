dd2fc35daf0cc3982badea9635caaacbf7e19151
Jeneggs <jenexnakaisumo@gmail.com>
2025-09-21 23:36:04 +0800

    feat(backend): added input sanitizations

--- login.php
+++ login.php
@@ -3,8 +3,8 @@ require_once 'db_connect.php';
 session_start();
 
 
-$user_name_input = "";
-$password_input = "";
+$user_name_input = htmlspecialchars($_POST['username']);
+$password_input = htmlspecialchars($_POST['password']);
 
 $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
 $stmt->bindParam(':username', $user_name_input);
