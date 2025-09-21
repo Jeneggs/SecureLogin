40b191244400cf31942f2d1dc1e884ffebde2fea
Jeneggs <jenexnakaisumo@gmail.com>
2025-09-21 22:44:35 +0800

    feat(backend): initializing prepared statements

--- login.php
+++ login.php
@@ -1,6 +1,14 @@
 <?php
-include'db_connect.php';
+require_once 'db_connect.php';
 session_start();
+
+
+$user_name_input = "";
+$password_input = "";
+
+$stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
+$stmt->bindParam(':username', $user_name_input);
+$stmt->execute();
 
 ?>
 
